b87f1f1eba5e8dbf7a21aa64b3e7c0fa974ef3a2
Amren Hossain <ashossain@umass.edu>
2025-05-06 17:09:10 -0400

    book1

---
+++ book1.py
@@ -0,0 +1,42 @@
+from __future__ import division
+import nltk
+import re
+import pprint
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import urllib.error
+
+nltk.download('punkt_tab')
+
+
+# Hardcoded URL of a book (Mostly What God Does: Reflections on Seeking and Finding His Love Everywhere) PW 3/3/2024
+url = "https://www.publishersweekly.com/9781401971366"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+
+try:
+    req = Request(url, headers=headers)
+    response = urlopen(req)
+    html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Print only the <div> tags and their contents
+    divs = soup.find('div', class_="text-body mdc-typography--body1 font-source-serif-pro")
+    for div in divs:
+        print(divs.prettify())  # This prints each <div> and its content nicely formatted
+
+except urllib.error.URLError as e:
+    print(f"URL Error: {e}")
+except urllib.error.HTTPError as e:
+    print(f"HTTP Error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+    
+    
+#tokenize the text
+if divs:
+    text = divs.get_text()
+    # Tokenize the text
+    tokens = nltk.word_tokenize(text)
+    print(tokens)
